3c36b347d9785e4b32b820ef7ebfef700f336177
Geovanna <silvarochageovanna4@gmail.com>
2026-06-28 22:45:22 -0300

    Refactor import statements and metric display

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import streamlit as st from google.oauth2 
-import service_account from google.cloud 
-import bigquery 
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import bigquery
 
-st.header("Indicadores") 
+st.header("Indicadores")
 
-col1, col2, col3 = st.columns(3) 
+col1, col2, col3 = st.columns(3)
 
-with col1: st.metric(label="Média de gols", value="00") 
-with col2: st.metric(label="Porc. de jogos sem derrotas", value="00") 
-with col3: st.metric(label="Partidas disputadas", value="00")
+with col1:
+    st.metric(label="Média de gols", value="00")
 
+with col2:
+    st.metric(label="Porc. de jogos sem derrotas", value="00")
+
+with col3:
+    st.metric(label="Partidas disputadas", value="00")
