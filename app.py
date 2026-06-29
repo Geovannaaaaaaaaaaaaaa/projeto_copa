27d9a05800903893095de33fc4ef5045f1fde74c
Geovanna <silvarochageovanna4@gmail.com>
2026-06-28 23:39:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,55 @@
 import streamlit as st
+from google.cloud import bigquery
 import pandas as pd
 
-from google.oauth2 import service_account
-from google.cloud import bigquery
+def get_bigquery_client():
+    credentials_info = st.secrets["gcp_service_account"]
+    return bigquery.Client.from_service_account_info(credentials_info)
+
+@st.cache_data(ttl=600)
+def query_media_gols():
+    client = get_bigquery_client()
+    query = "SELECT media_gols_brasil FROM `projeto-copa-500721.copa.vw_media-gols-brasil` LIMIT 1"
+    df = client.query(query).to_dataframe()
+    if df.empty:
+        return 0.0
+    return df[df.columns[0]].iloc[0]
+
+@st.cache_data(ttl=600)
+def query_porcentagem_nao_perdido():
+    client = get_bigquery_client()
+    query = "SELECT porcentagem_nao_perdido FROM `projeto-copa-500721.copa.vw_porcentagem-vitorias-empates-brasil` LIMIT 1"
+    df = client.query(query).to_dataframe()
+    if df.empty:
+        return 0.0
+    return df[df.columns[0]].iloc[0]
+
+@st.cache_data(ttl=600)
+def query_partidas_disputadas():
+    client = get_bigquery_client()
+    query = "SELECT numero_partidas FROM `projeto-copa-500721.copa.partidas-disputadas-brasil` LIMIT 1"
+    df = client.query(query).to_dataframe()
+    if df.empty:
+        return 0
+    return df[df.columns[0]].iloc[0]
+
+media_gols_valor = query_media_gols()
+porcentagem_nao_perdido_valor = query_porcentagem_nao_perdido()
+partidas_disputadas_valor = query_partidas_disputadas()
+
+media_gols_formatado = f"{media_gols_valor:.2f}"
+porcentagem_nao_perdido_formatado = f"{porcentagem_nao_perdido_valor}%" 
+partidas_disputadas_formatado = f"{partidas_disputadas_valor}"
+
+# Interface
 
 st.header("Indicadores") 
 
-col1, col2, col3 = st.columns(3) 
+col1, col2, col3 = st.columns(3)
 
-with col1: st.metric(label="Média de gols", value="00") 
-with col2: st.metric(label="Porc. de jogos sem derrotas", value="00") 
-with col3: st.metric(label="Partidas disputadas", value="00")
+with col1: 
+    st.metric(label="Média de gols", value=media_gols_formatado) 
+with col2: 
+    st.metric(label="% de jogos sem derrotas", value=porcentagem_nao_perdido_formatado) 
+with col3: 
+    st.metric(label="Partidas disputadas", value=partidas_disputadas_formatado)
