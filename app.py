9dfd54e280155b1071fb133e3a6b72d3bc07f7e7
Geovanna <silvarochageovanna4@gmail.com>
2026-06-28 23:16:02 -0300

    Fix import statement for BigQuery module

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 from google.oauth2 import service_account
-from google.cloud.bigquery import bigquery
+from google.cloud import bigquery
 
 st.header("Indicadores") 
 
