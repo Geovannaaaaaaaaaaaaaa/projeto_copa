7f0162c0f5a658f9f74429027b6bceb02510eb04
Geovanna <silvarochageovanna4@gmail.com>
2026-06-28 23:09:32 -0300

    Fix import statements in app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import streamlit as st from google.oauth2 
-import service_account from google.cloud 
-import bigquery 
+import streamlit as st
+import pandas as pd
 
+from google.oauth2 import service_account
+from google.cloud import bigquery
 st.header("Indicadores") 
 
 col1, col2, col3 = st.columns(3) 
